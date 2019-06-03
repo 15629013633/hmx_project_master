5a1b4fa33e191004392d0bcc6dbbab103c4ffb48
songjinbao123 <2428358848@qq.com>
2019-06-03 19:54:09 +0800

    aaa

--- hmx-manager/src/main/java/com/hmx/timer/ScheduledService.java
+++ hmx-manager/src/main/java/com/hmx/timer/ScheduledService.java
@@ -1,5 +1,8 @@
 package com.hmx.timer;
 
+import com.hmx.category.entity.HmxCategoryContentTrans;
+import com.hmx.category.service.HmxCategoryContentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -11,16 +14,19 @@ import org.springframework.stereotype.Component;
 @EnableScheduling
 public class ScheduledService {
 
+    @Autowired
+    private HmxCategoryContentService hmxCategoryContentService;
+
     @Scheduled(fixedRate = 2000)
     public void scheduled(){
-        System.out.println("111111$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        HmxCategoryContentTrans trans = hmxCategoryContentService.selectCategoryContentById(128);
+        //获取到所有的state=1的内容，如果检测到时间超过8小时则将state置为0
+        System.out.println("title="+trans.getCategoryTitle());
     }
-//    @Scheduled(fixedRate = 5000)
-//    public void scheduled1() {
-//        log.info("=====>>>>>使用fixedRate{}", System.currentTimeMillis());
-//    }
-//    @Scheduled(fixedDelay = 5000)
-//    public void scheduled2() {
-//        log.info("=====>>>>>fixedDelay{}",System.currentTimeMillis());
-//    }
+
+    @Scheduled(fixedRate = 4000)
+    public void scheduled1(){
+        System.out.println("******************************");
+    }
+
 }
